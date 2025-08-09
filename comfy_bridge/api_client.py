d58c57dbbe8cc3e22441d5ba9f2adbce546b6634
ameli0x <wright.amelio@outlook.com>
2025-08-08 23:23:22 -0500

    fix model map

--- comfy_bridge/api_client.py
+++ comfy_bridge/api_client.py
@@ -34,9 +34,7 @@ class APIClient:
             payload["models"] = models
 
         #logger.debug(f"pop_job sending payload: {payload}")
-        logger.debug(payload)
         try:
-            print(payload)
             response = await self.client.post(
                 "/v2/generate/pop", headers=self.headers, json=payload
             )
